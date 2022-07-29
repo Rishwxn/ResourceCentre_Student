7ae057d8b2fb8a1151b1d8d3d0eff09a6033b79d
21006685 <21006685@10.174.124.4>
2022-07-29 09:40:49 +0800

    OrderList

---
+++ ResourceCentre_Student/src/Order.java
@@ -0,0 +1,20 @@
+
+public class Order {
+	private String name;
+	private String id;
+	
+	public Order(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+}
